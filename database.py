c4b393195e0524bc347feb4e1462b93431e285f0
Aryan-cyber-guy <flyhigh7674@gmail.com>
2026-07-08 02:53:42 +0530

    Minor adjustments

--- database.py
+++ database.py
@@ -14,7 +14,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL environment variable is not set")
 
-engine = create_engine(DATABASE_URL, echo=True, pool_pre_ping=True)
+engine = create_engine(DATABASE_URL, echo=False, pool_pre_ping=True)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 Base.metadata.create_all(bind=engine)
--- main.py
+++ main.py
@@ -76,10 +76,11 @@ app.add_middleware(
     allow_origins=[
     "http://127.0.0.1:5500",
     "http://localhost:5500",
+    "https://vanilla-joy-shop.vercel.app"
     ],
-    allow_credentials=True,      # Note: Cannot be True if allow_origins=["*"] and sending cookies
-    allow_methods=["*"],            # Allows all HTTP methods (GET, POST, PUT, DELETE, etc.)
-    allow_headers=["*"],            # Allows all headers
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(cart.router, prefix="/cart")
